f10383d5dc90a18ac0ebf75e2ee11423d6ff9f39
Brian Firestone <bfirestone2339@gmail.com>
2021-03-30 18:40:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-\s-']+/g, '')
+  }
+
+  static titleize(string) {
+    const badWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let words = string.split(' ');
+    let result = [];
+    for (let i = 0; i < words.length; i++) {
+      if (i == 0) {
+        result.push(this.capitalize(words[i]))
+      } else {
+        if (badWords.includes(words[i])) {
+          result.push(words[i])
+        } else {
+          result.push(this.capitalize(words[i]))
+        }
+      }
+    }
+
+    return result.join(' ');
+
+  }
+
 }
